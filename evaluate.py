28ee5e0311fb1e0d4b298201b5f09bb7802e96a0
aviros <avirosman@gmail.com>
2019-08-22 17:55:02 +0300

    restore model

--- evaluate.py
+++ evaluate.py
@@ -7,22 +7,26 @@ import time
 import os
 import scipy.misc
 import sys
+import provider
+import rotationService
+import utils.pc_util
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, 'models'))
 sys.path.append(os.path.join(BASE_DIR, 'utils'))
-import provider
-import pc_util
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
-parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
+parser.add_argument('--model', default='pointnet_cls_basic', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
 parser.add_argument('--batch_size', type=int, default=4, help='Batch Size during training [default: 1]')
 parser.add_argument('--num_point', type=int, default=1024, help='Point Number [256/512/1024/2048] [default: 1024]')
 parser.add_argument('--model_path', default='log/model.ckpt', help='model checkpoint file path [default: log/model.ckpt]')
 parser.add_argument('--dump_dir', default='dump', help='dump folder path [dump]')
 parser.add_argument('--visu', action='store_true', help='Whether to dump image for error case [default: False]')
+parser.add_argument('--rotation_number', type=int, default=4, help='number of rotations for training')
+parser.add_argument('--debug_mode', type=bool, default=False, help='fast debug mode')
 FLAGS = parser.parse_args()
 
 
@@ -32,21 +36,29 @@ MODEL_PATH = FLAGS.model_path
 GPU_INDEX = FLAGS.gpu
 MODEL = importlib.import_module(FLAGS.model) # import network module
 DUMP_DIR = FLAGS.dump_dir
+ROTATION_NUMBER = FLAGS.rotation_number
+
 if not os.path.exists(DUMP_DIR): os.mkdir(DUMP_DIR)
 LOG_FOUT = open(os.path.join(DUMP_DIR, 'log_evaluate.txt'), 'w')
 LOG_FOUT.write(str(FLAGS)+'\n')
+PROFILE_DEBUG = FLAGS.debug_mode
+print('PROFILE_DEBUG is: ' + str(PROFILE_DEBUG))
+
 
 NUM_CLASSES = 40
 SHAPE_NAMES = [line.rstrip() for line in \
-    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/shape_names.txt'))] 
+    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/shape_names.txt'))]
 
 HOSTNAME = socket.gethostname()
 
 # ModelNet40 official train/test split
-TRAIN_FILES = provider.getDataFiles( \
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'))
-TEST_FILES = provider.getDataFiles(\
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/test_files.txt'))
+TRAIN_PATH = 'data/modelnet40_ply_hdf5_2048/train_files.txt'.replace('/','\\') if PROFILE_DEBUG \
+    else 'data/modelnet40_ply_hdf5_2048/train_files.txt'
+TEST_PATH = 'data/modelnet40_ply_hdf5_2048/test_files.txt'.replace('/','\\') if PROFILE_DEBUG \
+    else 'data/modelnet40_ply_hdf5_2048/test_files.txt'
+
+TRAIN_FILES = provider.getDataFiles(os.path.join(BASE_DIR, TRAIN_PATH))
+TEST_FILES = provider.getDataFiles(os.path.join(BASE_DIR, TEST_PATH))
 
 def log_string(out_str):
     LOG_FOUT.write(out_str+'\n')
@@ -55,18 +67,18 @@ def log_string(out_str):
 
 def evaluate(num_votes):
     is_training = False
-     
+
     with tf.device('/gpu:'+str(GPU_INDEX)):
-        pointclouds_pl, labels_pl = MODEL.placeholder_inputs(BATCH_SIZE, NUM_POINT)
+        pointclouds_pl, labels_pl = MODEL.placeholder_inputs(BATCH_SIZE, NUM_POINT, ROTATION_NUMBER)
         is_training_pl = tf.placeholder(tf.bool, shape=())
 
         # simple model
-        pred, end_points = MODEL.get_model(pointclouds_pl, is_training_pl)
+        pred, end_points = MODEL.get_model(pointclouds_pl, is_training_pl, ROTATION_NUMBER)
         loss = MODEL.get_loss(pred, labels_pl, end_points)
-        
+
         # Add ops to save and restore all the variables.
         saver = tf.train.Saver()
-        
+
     # Create a session
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -86,7 +98,7 @@ def evaluate(num_votes):
 
     eval_one_epoch(sess, ops, num_votes)
 
-   
+
 def eval_one_epoch(sess, ops, num_votes=1, topk=1):
     error_cnt = 0
     is_training = False
@@ -102,66 +114,58 @@ def eval_one_epoch(sess, ops, num_votes=1, topk=1):
         current_data = current_data[:,0:NUM_POINT,:]
         current_label = np.squeeze(current_label)
         print(current_data.shape)
-        
+
         file_size = current_data.shape[0]
         num_batches = file_size // BATCH_SIZE
         print(file_size)
-        
+        if PROFILE_DEBUG : num_batches = 15
         for batch_idx in range(num_batches):
             start_idx = batch_idx * BATCH_SIZE
             end_idx = (batch_idx+1) * BATCH_SIZE
             cur_batch_size = end_idx - start_idx
-            
+
             # Aggregating BEG
             batch_loss_sum = 0 # sum of losses for the batch
-            batch_pred_sum = np.zeros((cur_batch_size, NUM_CLASSES)) # score for classes
-            batch_pred_classes = np.zeros((cur_batch_size, NUM_CLASSES)) # 0/1 for classes
+            batch_pred_sum = np.zeros((cur_batch_size*ROTATION_NUMBER, ROTATION_NUMBER)) # score for classes
+            batch_pred_classes = np.zeros((cur_batch_size*ROTATION_NUMBER, ROTATION_NUMBER)) # 0/1 for classes
             for vote_idx in range(num_votes):
-                rotated_data = provider.rotate_point_cloud_by_angle(current_data[start_idx:end_idx, :, :],
-                                                  vote_idx/float(num_votes) * np.pi * 2)
+                rotated_data = (current_data[start_idx:end_idx, :, :])
+                # rotated_data = provider.rotate_point_cloud_by_angle(current_data[start_idx:end_idx, :, :],
+                #                                                     vote_idx / float(num_votes) * np.pi * 2)
+                rotation_labels = list(range(ROTATION_NUMBER))
+                rotation_labels = np.repeat(rotation_labels, end_idx - start_idx)
+                rotated_data = rotationService.get_images_rotation_according_to_rotation_label(rotated_data)
                 feed_dict = {ops['pointclouds_pl']: rotated_data,
-                             ops['labels_pl']: current_label[start_idx:end_idx],
+                             ops['labels_pl']: rotation_labels,
                              ops['is_training_pl']: is_training}
                 loss_val, pred_val = sess.run([ops['loss'], ops['pred']],
                                           feed_dict=feed_dict)
-                batch_pred_sum += pred_val
-                batch_pred_val = np.argmax(pred_val, 1)
-                for el_idx in range(cur_batch_size):
-                    batch_pred_classes[el_idx, batch_pred_val[el_idx]] += 1
-                batch_loss_sum += (loss_val * cur_batch_size / float(num_votes))
-            # pred_val_topk = np.argsort(batch_pred_sum, axis=-1)[:,-1*np.array(range(topk))-1]
-            # pred_val = np.argmax(batch_pred_classes, 1)
-            pred_val = np.argmax(batch_pred_sum, 1)
-            # Aggregating END
-            
-            correct = np.sum(pred_val == current_label[start_idx:end_idx])
-            # correct = np.sum(pred_val_topk[:,0:topk] == label_val)
-            total_correct += correct
-            total_seen += cur_batch_size
-            loss_sum += batch_loss_sum
+                pred_val = np.argmax(pred_val, 1)
+                correct = np.sum(pred_val == rotation_labels)
+                total_correct += correct
+                total_seen += BATCH_SIZE * ROTATION_NUMBER
+                loss_sum += (loss_val * ROTATION_NUMBER)
+                # for i in range(start_idx, end_idx):
+                #     l = current_label[i]
+                #     total_seen_class[l] += 1
+                #     total_correct_class[l] += (pred_val[i - start_idx] == l)
 
-            for i in range(start_idx, end_idx):
-                l = current_label[i]
-                total_seen_class[l] += 1
-                total_correct_class[l] += (pred_val[i-start_idx] == l)
-                fout.write('%d, %d\n' % (pred_val[i-start_idx], l))
-                
-                if pred_val[i-start_idx] != l and FLAGS.visu: # ERROR CASE, DUMP!
-                    img_filename = '%d_label_%s_pred_%s.jpg' % (error_cnt, SHAPE_NAMES[l],
-                                                           SHAPE_NAMES[pred_val[i-start_idx]])
-                    img_filename = os.path.join(DUMP_DIR, img_filename)
-                    output_img = pc_util.point_cloud_three_views(np.squeeze(current_data[i, :, :]))
-                    scipy.misc.imsave(img_filename, output_img)
-                    error_cnt += 1
-                
+                # if pred_val[i-start_idx] != l and FLAGS.visu: # ERROR CASE, DUMP!
+                #     img_filename = '%d_label_%s_pred_%s.jpg' % (error_cnt, SHAPE_NAMES[l],
+                #                                            SHAPE_NAMES[pred_val[i-start_idx]])
+                #     img_filename = os.path.join(DUMP_DIR, img_filename)
+                #     output_img = pc_util.point_cloud_three_views(np.squeeze(current_data[i, :, :]))
+                #     scipy.misc.imsave(img_filename, output_img)
+                #     error_cnt += 1
+
     log_string('eval mean loss: %f' % (loss_sum / float(total_seen)))
     log_string('eval accuracy: %f' % (total_correct / float(total_seen)))
     log_string('eval avg class acc: %f' % (np.mean(np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float))))
-    
+
     class_accuracies = np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float)
     for i, name in enumerate(SHAPE_NAMES):
         log_string('%10s:\t%0.3f' % (name, class_accuracies[i]))
-    
+
 
 
 if __name__=='__main__':
