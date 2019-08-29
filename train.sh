1b8fe0f97f386e366de4c1e7a07b5309c4da6bca
aviros <avirosman@gmail.com>
2019-08-29 17:09:39 +0300

    script

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
-python trainRotation.py --model_save_path=fourRotations
-python trainClasiffiers.py \
+!python trainRotation.py --model_save_path=fourRotations
+!python trainClasiffiers.py \
 --model_save_path=fc3_stop_gradient_4rotations \
 --model_restore_path=fourRotations \
 --fc_layers_number=3 \
