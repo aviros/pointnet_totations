63e4e5f1543e9fa3de33341248f7de4e04e9403a
aviros <avirosman@gmail.com>
2019-09-07 23:23:31 +0300

    script

--- provider.py
+++ provider.py
@@ -13,7 +13,7 @@ if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
     www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
     zipfile = os.path.basename(www)
     os.system('wget %s; unzip %s' % (www, zipfile))
-    os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
+    os.system('mv %s %s' % (zipfile[:-4], DATA_DIR.replace("\\", "")))
     os.system('rm %s' % (zipfile))
 
 
